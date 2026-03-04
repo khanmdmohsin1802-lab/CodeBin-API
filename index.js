5b674dd8502679a9d147f62f41a020f20985ab8f
MohsinKhan <khanmdmohsin1802@gmail.com>
2026-03-04 23:16:22 +0530

    feat: added helmet and express-rate-limit for API security

--- index.js
+++ index.js
@@ -5,12 +5,30 @@ import mongoose from "mongoose";
 import userRoute from "./routes/user.js";
 import snippetRoute from "./routes/snippet.js";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import ratelimit from "express-rate-limit";
+
 const app = express();
 
 mongoose
   .connect(process.env.MONGOURL)
   .then(() => console.log("✅ mongoose connected"))
   .catch((err) => console.log(err));
+
+app.use(helmet());
+
+const limiter = ratelimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  message: {
+    error: "To many messages",
+    msg: "You have been rate limited",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,5 +37,5 @@ app.use(cookieParser());
 app.use("/user", userRoute);
 app.use("/snippet", snippetRoute);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log("Running at port : http://localhost:8001"));
